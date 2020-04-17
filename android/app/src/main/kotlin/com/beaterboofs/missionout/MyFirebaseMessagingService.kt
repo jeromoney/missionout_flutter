1dba64d18838f24e6341b117e7bf11e65cc58244
jeromoney <justin.matis.com@gmail.com>
2020-04-16 20:34:23 -0600

    fixed non working sound again

--- android/app/src/main/kotlin/com/beaterboofs/missionout/MyFirebaseMessagingService.kt
+++ android/app/src/main/kotlin/com/beaterboofs/missionout/MyFirebaseMessagingService.kt
@@ -1,7 +1,6 @@
 package com.beaterboofs.missionout
 
 import android.annotation.SuppressLint
-import android.app.Notification
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
@@ -10,7 +9,6 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.media.AudioAttributes
-import android.media.RingtoneManager
 import android.net.Uri
 import android.os.Build
 import androidx.core.app.NotificationCompat
@@ -23,12 +21,11 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
     private val CHANNEL_ID = "1234"
     private val CHANNEL_NAME = "Mission Pages"
 
-    private fun createNotificationChannel(): NotificationManager? {
+    private fun createNotificationChannel(defaultSoundUri: Uri) {
         // Create the NotificationChannel, but only on API 26+ because
         // the NotificationChannel class is new and not in the support library
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val defaultSoundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE+ "://"+ getApplicationContext().getPackageName() + "/" + R.raw.school_fire_alarm)
             val audioAttributes = AudioAttributes.Builder()
                     .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
                     .setUsage(AudioAttributes.USAGE_NOTIFICATION)
@@ -42,11 +39,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
                 setSound(defaultSoundUri, audioAttributes)
             }
             notificationManager.createNotificationChannel(channel)
-            return notificationManager
-
-
-        } else {
-            return null
         }
     }
 
@@ -54,7 +46,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
         super.onMessageReceived(message)
         // Uh Oh -- FCM received so there must be a mission.
         // Display a notification, play sound and allow user to dismiss sound
-        val notificationManager = createNotificationChannel()
+        val defaultSoundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE + "://" + getApplicationContext().getPackageName() + "/" + R.raw.school_fire_alarm)
+        createNotificationChannel(defaultSoundUri)
 
         val header = message.data["description"]
         val body = message.data["action"]
@@ -66,27 +59,6 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         val notificationIntent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
         val pendingIntent = PendingIntent.getActivity(baseContext, 0, notificationIntent, 0)
-
-        //val notificationManager = applicationContext.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        // play sound
-        //val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM)
-//        val defaultSoundUri = Uri.parse(ContentResolver.SCHEME_ANDROID_RESOURCE+ "://"+ getApplicationContext().getPackageName() + "/" + R.raw.school_fire_alarm)
-//        val channel: NotificationChannel
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH).apply {
-//                lightColor = Color.GRAY
-//                enableLights(true)
-//
-//                description = "Page Channel"
-//            }
-//            val audioAttributes = AudioAttributes.Builder()
-//                    .setContentType(AudioAttributes.CONTENT_TYPE_UNKNOWN)
-//                    .setUsage(AudioAttributes.USAGE_NOTIFICATION)
-//                    .build()
-//            channel.setSound(defaultSoundUri, audioAttributes)
-//            notificationManager.createNotificationChannel(channel)
-//        }
-
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
                 .setSmallIcon(R.drawable.notification_logo)
                 .setContentTitle(header)
@@ -95,13 +67,12 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
                 .setContentIntent(pendingIntent)
                 .setDeleteIntent(pendingIntent)
                 .setSmallIcon(R.drawable.notification_logo)
-                .build()
+                .setSound(defaultSoundUri) // .setSound only works on android<26
 
-        notificationManager!!.notify(notificationId, notification)
         // show notification
-//        with(NotificationManagerCompat.from(this)) {
-//            // notificationId is a unique int for each notification that you must define
-//            notify(notificationId, builder.build())
-//        }
+        with(NotificationManagerCompat.from(this)) {
+            // notificationId is a unique int for each notification that you must define
+            notify(notificationId, notification.build())
+        }
     }
 }
