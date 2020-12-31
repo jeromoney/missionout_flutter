b39fdfbed08ca372c729b76e5a5242686fac9144
Justin <justin.matis.com@gmail.com>
2020-12-31 11:42:49 -0700

    Going fo rmy long lost todo

--- lib/app/sign_in/LoginScreen/log_in_screen.dart
+++ lib/app/sign_in/LoginScreen/log_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:device_info/device_info.dart';
 import 'package:email_validator/email_validator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_auth_buttons/flutter_auth_buttons.dart';
@@ -20,10 +21,13 @@ class LogInScreen extends StatefulWidget {
 
 class _LogInScreenState extends State<LogInScreen> {
   AppSetup _appSetup;
+
   String get gmailDomain => _appSetup?.gmailDomain;
   bool get showGoogleButton => true;
-  bool get showAppleButton => _appSetup?.showAppleButton ?? Platforms.isIOS;
+  bool showAppleButton = false;
+
   bool get showEmailLogin => _appSetup?.showEmailLogin ?? false;
+
   @override
   void initState() {
     super.initState();
@@ -31,8 +35,20 @@ class _LogInScreenState extends State<LogInScreen> {
     _setEmailField(context);
   }
 
+  // Apple Setup - Show Apple button only if iOS and running 13 or later. Since
+  // this method is async it needs to be used with a future builder
+  Future<bool> _isCorrectIosVersion() async {
+    if (Platforms.isIOS) {
+      final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      final IosDeviceInfo iosInfo = await deviceInfo.iosInfo;
+      final systemVersion = iosInfo.systemVersion;
+      return systemVersion.startsWith(RegExp("1[3,4,5,6,7,8,9]\."));
+    }
+    return false;
+  }
+
   // TODO - refactor this with futurebuilder,
-  void _setEmailField(BuildContext context) async {
+  _setEmailField(BuildContext context) async {
     widget._emailController.text = await LoginScreenModel.getEmail(context);
     setState(() {});
   }
@@ -51,7 +67,9 @@ class _LogInScreenState extends State<LogInScreen> {
               if (details.delta.dx > 0) Navigator.of(context).pop();
             },
             child: Scaffold(
-              appBar: AppBar(title: Text("MissionOut Log In"),),
+              appBar: AppBar(
+                title: Text("MissionOut Log In"),
+              ),
               body: Center(
                 child: LayoutBuilder(
                   builder: (context, viewportConstraints) =>
@@ -70,17 +88,25 @@ class _LogInScreenState extends State<LogInScreen> {
                               onPressed: () => model.signInWithGoogle(
                                   hostedDomain: gmailDomain),
                             ),
-                          if (model.isAppleSignInAvailable &&
-                              showAppleButton) ...[
-                            AppleSignInButton(
-                              text: 'Log in with Apple',
-                              style: AppleButtonStyle.white,
-                              onPressed: model.signInWithApple,
-                            )
+                          if (model.isAppleSignInAvailable) ...[
+                            FutureBuilder(
+                              future: _isCorrectIosVersion(),
+                              initialData: false,
+                              builder: (context, snapshot) {
+                                if (snapshot.hasData && snapshot.data) {
+                                  return AppleSignInButton(
+                                    text: 'Log in with Apple',
+                                    style: AppleButtonStyle.white,
+                                    onPressed: model.signInWithApple,
+                                  );
+                                } else {
+                                  return Container();
+                                }
+                              },
+                            ),
                           ],
                           if (showEmailLogin &&
-                              (showAppleButton ||
-                                  showGoogleButton))
+                              (showAppleButton || showGoogleButton))
                             Divider(),
                           if (showEmailLogin)
                             Form(
